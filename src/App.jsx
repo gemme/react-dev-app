232bca1309dd73594a886f364ba9f0e8e21243e9
Ernesto Martinez Escobar <gagamart@Gabriels-MacBook-Pro.local>
2024-03-06 16:54:36 -0600

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,53 @@
 import './App.css'
 import 'semantic-ui-css/semantic.min.css'
-import { ApprovalCard } from './components/ApprovalCard';
-import { CommentRow } from './components/CommentRow';
+import Root from './routes/Root';
+import {StarWars} from './routes/StarWars';
+import { InboxMessages } from './routes/InboxMessages';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ErrorPage from './routes/ErrorPage';
+import { DigitalClock } from './routes/DigitalClock';
+import { Calculator } from './routes/Calculator';
+import { Flicker } from './routes/Flicker';
+import { Home } from './routes/Home';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "star-wars/",
+        element: <StarWars />,
+      }, 
+      {
+        path: "inbox/",
+        element: <InboxMessages />,
+      },
+      {
+        path: "digital-clock/",
+        element: <DigitalClock />,
+      },{
+        path: "calculator/",
+        element: <Calculator />,
+      },
+      {
+        path: "flicker/",
+        element: <Flicker />,
+      }
+    ],
+  }
+]);
+
+
 function App() {
   return (
     <>
-      <ApprovalCard >
-        <CommentRow />
-      </ApprovalCard>
-      <ApprovalCard >
-        <CommentRow />
-      </ApprovalCard>
-      <ApprovalCard >
-        <CommentRow />
-      </ApprovalCard>
+      <RouterProvider router={router} />   
     </>
   )
 }
---
+++ src/routes/DigitalClock.jsx
@@ -0,0 +1,8 @@
+export const DigitalClock = () => {
+    return (
+        <div>
+            <h1>Digital Clock</h1>
+            <h2>{new Date().toLocaleTimeString()}</h2>
+        </div>
+    )
+};
